aced8259ca5bb1cc2cc4db3f9b35b3feb26ae689
rhardi1 <rachel.hardin@jefferson.kyschools.us>
2021-10-15 11:59:11 -0400

    why is this not working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,29 @@
-const landing = document.getElementById("landing");
+const typingh1 = document.getElementById("typing-h1");
+const typingSpan = document.getElementById("typing-span");
 
-window.onload = function(){
-    setTimeout(function(){
-        var i = 0;
-        var msg = '\'Hello, world!\''
-            function typing(){
-                if (i < msg.length){
-                landing.innerHTML += msg.charAt(i);
-                i++;
-                setTimeout(typing, 50);
+// window.onload = setTimeout(typing, 2000);
+window.onload = typing();
+
+function typing() {
+    let msg = "Hello, world!";
+    for (let i = 0; i < (msg.length); i++) {
+        setTimeout(function () {
+            typingh1.innerHTML += msg.charAt(i);
+        }, 50);
+    }
+    setTimeout(function () {
+        var fadeOut = setInterval(function () {
+            if (!typingSpan.style.opacity) {
+                typingSpan.style.opacity = 1;
             }
-        }
-    }, 1000);
+
+            if (typingSpan.style.opacity > 0) {
+                typingSpan.style.opacity -= 0.1;
+            }
+
+            else {
+                clearInterval(fadeOut);
+            }
+        }, 200);
+    }, 4000);
 }
